343f8b7d5d9e39c41c676e2d3740175c7140f436
michaelratke <michael@ratke.info>
2026-02-18 23:22:56 +0100

    chore: update `base.schema.ts` to use `uuid` format for `_id`, `locationId`, and `tenantId`

--- libs/shared/common/src/lib/schemas/base.schema.ts
+++ libs/shared/common/src/lib/schemas/base.schema.ts
@@ -1,11 +1,11 @@
-import { ObjectIdSchema, Type } from '@feathersjs/typebox'
+import { Type } from '@feathersjs/typebox'
 
 export const baseSchema = {
-  _id: ObjectIdSchema(), // Database ID (optional)
+  _id: Type.String({ format: 'uuid' }), // Database ID (optional)
 
   createdAt: Type.Optional(Type.String({ format: 'date-time' })), // Creation date
   updatedAt: Type.Optional(Type.String({ format: 'date-time' })), // Change date
 
-  locationId: ObjectIdSchema(), // Location affiliation
-  tenantId: ObjectIdSchema(), // Organizational affiliation
+  locationId: Type.String({ format: 'uuid' }), // Location affiliation
+  tenantId: Type.String({ format: 'uuid' }), // Organizational affiliation
 }

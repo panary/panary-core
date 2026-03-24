9917512684eca182f8f252f388f15efdaf656655
michaelratke <michael@ratke.info>
2026-03-24 18:50:14 +0100

    fix: add TranslateModule import to app test setup

--- apps/setup-client/src/app/app.spec.ts
+++ apps/setup-client/src/app/app.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing'
 import { App } from './app'
+import { TranslateModule } from '@ngx-translate/core'
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, TranslateModule.forRoot()],
     }).compileComponents()
   })
 
